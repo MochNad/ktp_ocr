83cdfc241be224056d5ea87bad6d4ccce584ee4d
MochNad <mochammadnadiraflimaulana@gmail.com>
2023-12-21 21:31:45 +0700

    add more normalized ktp api and remake button flutter

--- app/app_ktp_ocr/lib/main.dart
+++ app/app_ktp_ocr/lib/main.dart
@@ -4,26 +4,27 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
+import 'package:line_icons/line_icons.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, // Menghilangkan tulisan debug
-      theme: ThemeData.dark(), // Mengaktifkan dark mode
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
       home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key});
 
   @override
   MyHomePageState createState() => MyHomePageState();
@@ -35,29 +36,25 @@ class MyHomePageState extends State<MyHomePage> {
 
   Future<void> _uploadImage() async {
     if (_image == null) {
-      return;
+      return Future.value(); // Added return statement here
     }
 
-    String apiUrl = "http://192.168.68.104:5000/result";
+    String apiUrl = "http://192.168.158.12:5000/result";
     var uri = Uri.parse(apiUrl);
 
-    // Create multipart request
     var request = http.MultipartRequest("POST", uri);
     request.files.add(await http.MultipartFile.fromPath('file', _image!.path));
 
     try {
       var response = await request.send();
       if (response.statusCode == 200) {
-        // Successfully uploaded, parse the JSON response
         var responseBody = await response.stream.bytesToString();
         var result = json.decode(responseBody);
 
-        // Update the state with the result
         setState(() {
           _result = jsonEncode(result);
         });
 
-        // Show result in a dialog
         showDialog(
           context: context,
           builder: (BuildContext context) {
@@ -96,12 +93,14 @@ class MyHomePageState extends State<MyHomePage> {
           },
         );
       } else {
-        // Handle error
         print("Error: ${response.reasonPhrase}");
       }
     } catch (error) {
       print("Error: $error");
     }
+
+    // Add the following return statement to address the warning
+    return Future.value();
   }
 
   Widget buildField(String label, String value) {
@@ -146,18 +145,31 @@ class MyHomePageState extends State<MyHomePage> {
                 ? const Text("Pilih gambar KTP terlebih dahulu")
                 : Image.file(_image!),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _getImage,
-              child: const Text("Pilih Gambar KTP"),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _uploadImage,
-              child: const Text("Unggah dan Proses OCR"),
-            ),
           ],
         ),
       ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _image == null
+                ? null
+                : () async {
+                    await _uploadImage();
+                  },
+            tooltip: 'Unggah dan Proses OCR',
+            child: const Icon(Icons.upload),
+            backgroundColor: _image == null ? Colors.grey : null,
+          ),
+          const SizedBox(height: 16),
+          FloatingActionButton(
+            onPressed: _getImage,
+            tooltip: 'Pilih Gambar KTP',
+            child: Icon(LineIcons.plus),
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
